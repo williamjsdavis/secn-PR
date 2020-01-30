31d624ea38cc6fec57db9bf0b2ee2a372821a577
william davis <williamjsdavis@berkeley.edu>
2020-01-29 17:48:48 -0800

    Added FitOptionsClass

---
+++ FitOptionsClass.m
@@ -0,0 +1,40 @@
+classdef FitOptionsClass
+%Fit option class
+%   William Davis, 27/01/20
+%
+%   Notes:
+%   Defining class for calculation options for estimating correlation time
+%   and drift and noise functions from conditional moments. This class has
+%   no non-default value necessary properties.
+%
+%   Inputs:
+%   - "thetaConvergence"        Iteration error limit for theta, double
+%   - "functionConvergence"     Same for drift and noise functions, double
+%   - "keepObservations"        Keep observationData? logical
+%   - "printOutput"             Print to command window? logical
+%
+%   Problems:
+%   - 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    properties
+        thetaConvergence(1,1) double {mustBeNumeric,mustBeFinite,...
+            mustBeNonempty,mustBeReal,mustBePositive} = 1E-2
+        functionConvergence(1,1) double {mustBeNumeric,mustBeFinite,...
+            mustBeNonempty,mustBeReal,mustBePositive} = 0.2
+        keepObservations(1,1) logical = true
+        printOutput(1,1) logical = true
+    end
+    
+    methods
+        function obj = FitOptionsClass()
+            %%%Constructor method, initialise necessary properties
+            
+        end
+        
+%         function outputArg = method1(obj,inputArg)
+%             %METHOD1 Summary of this method goes here
+%             %   Detailed explanation goes here
+%             outputArg = obj.Property1 + inputArg;
+%         end
+    end
+end
