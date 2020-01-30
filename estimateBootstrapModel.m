e219a303dc82282e076d0f5dc7fb1d8ea432fe46
william davis <williamjsdavis@berkeley.edu>
2020-01-29 17:47:50 -0800

    Added estimators

---
+++ estimateBootstrapModel.m
@@ -0,0 +1,309 @@
+function outSPbootstrapObject = estimateBootstrapModel(SPmodel,bootstrapOptions)
+%Estimating bootstrapped stochastic process model 
+%   William Davis, 21/01/20
+%
+%   Notes:
+%   Takes estimated stochastic process model and uses a block bootstrap
+%   algorithm to estimate uncertainties on correlation time and drift and
+%   noise function parameters.
+%
+%   Inputs:
+%   - "SPmodel"                 Stochastic process model object, 
+%                                   SPmodelClass
+%   - "bootstrapOptions"        Options, BootstrapOptionsClass
+%       - "blockLength"         Length of blocks, double
+%       - "nBootstrapSamples"   Number of bootstrap samples, double
+%       - "parallelProcess"     Switch for parallel processing, logical
+%
+%   Problems:
+%   - Recover fit errors?
+%   - Add option to surpress print to terminal?
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Printing to terminal
+disp('Starting correlated process regression and bootstrap iterations')
+disp(['Number of data sources: ',num2str(...
+    SPmodel.momentData.observationData.dataNumber)])
+disp(['Total number of data points: ',num2str(...
+    SPmodel.momentData.observationData.dataTotalLength)])
+disp(['Parallel processing: ',mat2str(bootstrapOptions.parallelProcess)])
+%disp(['Kernel bandwidth: ',response.h])
+disp(['Number of bootstrap iterations: ',...
+    num2str(bootstrapOptions.nBootstrapSamples)])
+disp(['Bootstrap block length: ',num2str(bootstrapOptions.blockLength)])
+disp('Starting calculations')
+
+%% Bootstrapping uncertainties
+% Get settings
+handleSPmodel = SPmodel;
+handleSPmodel.fitOptions.keepObservations = false;
+handleSPmodel.fitOptions.printOutput = false;
+handleBootstrapOptions = BootstrapOptionsClass(0);
+
+% Input function
+SPhandle = @(x) fullSPestimate(x,handleSPmodel.momentData.momentOptions,...
+    handleSPmodel.fitOptions,handleBootstrapOptions);
+
+% Full data estimate
+originalEstimate = {SPmodel.correlationEstimate,SPmodel.driftEstimate,...
+    SPmodel.noiseEstimate}; 
+
+% Bootstrapping
+[unbiasedBootstrapDistributions,meanAbsoluteError] = blockBootstrap(...
+    SPmodel.momentData.observationData,...
+    SPhandle,originalEstimate,bootstrapOptions.blockLength,...
+    bootstrapOptions.nBootstrapSamples,...
+    bootstrapOptions.parallelProcess);
+
+%% Bootstrap results statistics
+[standardErrors,percentiles95] = calculateBootstrapStatistics(...
+    unbiasedBootstrapDistributions);
+
+% Finished
+disp('Finished calculations')
+
+%% Making object
+outSPbootstrapObject = SPbootstrapClass(unbiasedBootstrapDistributions,...
+    standardErrors,percentiles95,meanAbsoluteError,SPmodel,...
+    bootstrapOptions);
+end
+function [standardErrors,percentiles95] = ...
+    calculateBootstrapStatistics(distributions)
+%Getting default bootstrapping settings
+%   William Davis, 21/01/20
+%
+%   Notes:
+%   Calculating statistics on bootstrap estimates. Standard error, and 95%
+%   percentiles are reported.
+%
+%   Inputs:
+%   - "distributions"           Distributions of correlation time and 
+%                               functions, structure
+%
+%   Problems:
+%   - 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Standard errors
+standardErrors.correlationEstimate = ...
+    std(distributions.correlationEstimate);
+standardErrors.driftEstimate = std(distributions.driftEstimate);
+standardErrors.noiseEstimate = std(distributions.noiseEstimate);
+
+%% Percentiles (95%)
+percentileLevel = 95;
+percentileLowerBound = (100-percentileLevel)/2;
+percentileUpperBound = (100+percentileLevel)/2;
+
+percentiles95.correlationEstimate(2) = ...
+    prctile(distributions.correlationEstimate,percentileLowerBound);
+percentiles95.correlationEstimate(1) = ...
+    prctile(distributions.correlationEstimate,percentileUpperBound);
+percentiles95.driftEstimate(2,:) = ...
+    prctile(distributions.driftEstimate,percentileLowerBound);
+percentiles95.driftEstimate(1,:) = ...
+prctile(distributions.driftEstimate,percentileUpperBound);
+percentiles95.noiseEstimate(2,:) = ...
+prctile(distributions.noiseEstimate,percentileLowerBound);
+percentiles95.noiseEstimate(1,:) = ...
+    prctile(distributions.noiseEstimate,percentileUpperBound);
+end
+function [unbiasedBootstrapDistributions,meanAbsoluteError] = ...
+    blockBootstrap(observationData,fullFunctionHandle,originalEstimate,...
+    blockLength,nBootstrapSamples,parallelProcess)
+%Block bootstrap uncertainties
+%   William Davis, 15/07/19
+%
+%   Notes:
+%   Block bootstrap algorithm. Based on moving block bootstrap method of 
+%   Kunsch (1989):
+%
+%   The Jackknife and the Bootstrap for General Stationary Observations,
+%   The Annals of Statistics, Vol. 17, No. 3 (Sep., 1989), pp. 1217-1241
+%
+%   Inputs:
+%   - "observationData"         Observation data object, ObservationClass
+%   - "fullFunctionHandle"      Function of data to result, function_handle
+%   - "originalEstimate"        Original data estimate, cell
+%   - "blockLength"             Length of blocks, double
+%   - "nBootstrapSamples"       N. of bootstrap iterations, double
+%   - "nEvalPoints"             N. points for spatial sampling, double
+%   - "parallelProcess"         Use parallel processing? logical
+%
+%   Problems:
+%   - Check biases
+%   - Explore spmd option?
+%   - Add option to surpress print to terminal?
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Bootstrap sampling
+% Make method
+drawFunction = makeBootstrapMethod(observationData.dataNumber,...
+    observationData.dataLength,blockLength);
+
+% Draw sample and pass to function (can be done parallel)
+bootstrapEstimate = cell(nBootstrapSamples,1); % Preallocate
+meanAbsoluteError = zeros(nBootstrapSamples,1);
+if parallelProcess
+    parfor n = 1:nBootstrapSamples
+        disp(['Draw: ',num2str(n)])
+        [bootstrapEstimate{n},meanAbsoluteError(n)] = ...
+            parallelBootstrap(observationData,...
+            drawFunction,fullFunctionHandle);
+    end
+else
+    for n = 1:nBootstrapSamples
+        disp(['Draw: ',num2str(n)])
+        [bootstrapEstimate{n},meanAbsoluteError(n)] = ...
+            parallelBootstrap(observationData,...
+            drawFunction,fullFunctionHandle);
+    end
+end
+
+%% Make distributions
+bootstrapDistributions = makeDistributions(bootstrapEstimate,...
+    nBootstrapSamples);
+
+%% Correct for bias
+unbiasedBootstrapDistributions = correctBootstrapBias(...
+    bootstrapDistributions,originalEstimate,blockLength);
+end
+function drawFunction = makeBootstrapMethod(dataNumber,dataLength,...
+    blockLength)
+%Making bootstrap sampling method
+%   William Davis, 23/01/20
+%
+%   Notes:
+%   Methodology for bootstrap sampling. Creates a function handle that
+%   creates random bootstrap samples for the input data observations.
+%
+%   Inputs:
+%   - "dataNumber"              Number of observations, double
+%   - "dataLength"              Observation lengths, vector
+%   - "blockLength"             Length of blocks, double
+%
+%   Problems:
+%   -
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Sampling functions
+rdraw = @(N) randi(N,1,ceil(N/blockLength)); % Random draw
+drawin = @(x) x:(x+blockLength-1); % Select indices of blocks
+moddraw = @(x,N) mod(x-1,N) + 1; % Modulo
+drawN = @(x,N) moddraw(x(1:N),N); % Cut after N samples
+allin = @(x) cell2mat(arrayfun(drawin,x,'UniformOutput',false)); % Concat.
+bsdrawi = @(N) drawN(allin(rdraw(N)),N); % Draw pseudo sample indices
+
+%% Making function handle
+drawFunction = cell(1,dataNumber);
+for nd = 1:dataNumber
+    drawFunction{nd} = @(x) bsdrawi(dataLength(nd));
+end
+end
+function [bootstrapEstimate,meanAbsoluteError] = parallelBootstrap(...
+    observationData,drawFunction,fullFunctionHandle)
+%Parallel bootstrap function
+%   William Davis, 17/07/19
+%
+%   Notes:
+%   Parallel passing of data for bootstrap sampling and function
+%   evaluation. Written in this way to avoid MATLAB thinking there are
+%   broadcast variables.
+%
+%   Inputs:
+%   - "observationData"         Observation data object, ObservationClass
+%   - "drawFunction"            Bootstrap draw function, function_handle
+%   - "fullFunctionHandle"      Function of data to result, function_handle
+%
+%   Problems:
+%   -
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Processing
+% Make bootstrap sample
+bootstrapData = makeBootstrapSample(observationData,drawFunction);
+
+% Function of bootstrap data
+bootstrapAllResults = fullFunctionHandle(bootstrapData);
+bootstrapEstimate = {bootstrapAllResults.correlationEstimate,...
+    bootstrapAllResults.driftEstimate,bootstrapAllResults.noiseEstimate};
+meanAbsoluteError = bootstrapAllResults.meanAbsoluteError.bothMoments;
+end
+function bootstrapData = makeBootstrapSample(observationData,drawFunction)
+%Making bootstrap sample data
+%   William Davis, 22/01/20
+%
+%   Notes:
+%   Takes original observations and makes a block bootstrap sample to be
+%   used as surrogate data.
+%
+%   Inputs:
+%   - "observationData"         Observation data object, ObservationClass
+%   - "drawFunction"            Bootstrap draw function, function_handle
+%
+%   Problems:
+%   - 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Processing
+cellData = arrayfun(@(n) observationData.dataCell{n}(drawFunction{n}()),...
+    1:2,'UniformOutput',false);
+
+% Making object
+bootstrapData = ObservationClass(observationData.timeStep,cellData);
+end
+function distributions = makeDistributions(bootstrapEstimate,...
+    nBootstrapSamples)
+%Extract distributions from bootstrap results
+%   William Davis, 23/01/20
+%
+%   Notes:
+%   Extract results for correlation time and drift and noise functions for
+%   each bootstrap result, and make into a distribution. Distributions are
+%   either vectors or matricies.
+%
+%   Inputs:
+%   - "bootstrapEstimate"       Cell array of bootstrap results, cell
+%   - "bootstrapSamples"        Number of bootstrap iterations, double
+%   - "nEvalPoints"             N. points for spatial sampling, double
+%
+%   Problems:
+%   -
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Processing correlation time
+distributions.correlationEstimate = ...
+    arrayfun(@(n) bootstrapEstimate{n}{1},1:nBootstrapSamples);
+
+%% Processing functions
+distributions.driftEstimate = ...
+    cell2mat(cellfun(@(x) x{2},bootstrapEstimate,'UniformOutput',false));
+distributions.noiseEstimate = ...
+    cell2mat(cellfun(@(x) x{3},bootstrapEstimate,'UniformOutput',false));
+end
+function unbiasedDistributions = correctBootstrapBias(...
+    bootstrapDistributions,originalEstimate,blockLength)
+%Correct for block size bias 
+%   William Davis, 16/07/19
+%
+%   Notes:
+%   Scales result from block bootstrap sample; adjusting for bias. 
+%
+%   Inputs:
+%   - "bootstrapDistributions"  Distribution of bootstrap results, matrix
+%   - "originalEstimate"        Original data estimate, cell
+%   - "blockLength"             Length of blocks (not used yet), double
+%
+%   Problems:
+%   - 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Processing correlation time
+correlationEstimateBias = ...
+    mean(bootstrapDistributions.correlationEstimate) - originalEstimate{1};
+unbiasedDistributions.correlationEstimate = ...
+    bootstrapDistributions.correlationEstimate - correlationEstimateBias;
+
+%% Processing functions
+driftEstimateBias = mean(bootstrapDistributions.driftEstimate) - ...
+    originalEstimate{2};
+unbiasedDistributions.driftEstimate = ...
+    bootstrapDistributions.driftEstimate - driftEstimateBias;
+
+noiseEstimateBias = mean(bootstrapDistributions.noiseEstimate) - ...
+    originalEstimate{3};
+unbiasedDistributions.noiseEstimate = ...
+    bootstrapDistributions.noiseEstimate - noiseEstimateBias;
+end
---
+++ estimateSPmodel.m
@@ -0,0 +1,415 @@
+function outSPmodelObject = estimateSPmodel(momentData,fitOptions)
+%Estimate stochastic process model
+%   William Davis, 20/01/20
+%
+%   Notes:
+%   Takes moments object and fits for correlation time, theta, and 
+%   drift, f(x), and noise, g(x), functions. Theory from:
+%
+%   Lehle & Peinke, 2018
+%   Analyzing a stochastic process driven by Ornstein-Uhlenbeck noise
+%   PHYSICAL REVIEW E 97, 012113 (2018)
+%
+%   Inputs:
+%   - "momentData"              Moment data object, MomentClass
+%   - "fitOptions"              Options, FitOptionsClass
+%       - "thetaConvergence"    Iteration error limit for theta, double
+%       - "functionConvergence" Same for drift and noise functions, double
+%       - "keepObservations"    Keep observationData? logical
+%       - "printOutput"         Print to command window? logical
+%
+%   Problems:
+%   - Alter theta_search.m to take arbitrary timeShiftSamplePoints vector.
+%   - Alter theta_search.m to give rMatrix for vector above...
+%   - Removed possible change in lambda_tau_choice. Investigate further.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Estimate correlation time (theta)
+% Maximum correlation time
+maximumLag = max(momentData.momentOptions.timeShiftSamplePoints);
+maximumTheta = maximumLag*momentData.observationData.timeStep;
+
+% Estimate correlation time
+[thetaStar,thetaProperties] = theta_search(...
+    momentData.observationData.dataCell,...
+    momentData.observationData.timeStep,maximumLag,maximumTheta,...
+    fitOptions.thetaConvergence);
+
+%% Estimate drift and noise functional form
+% Estimation of lambda vector (linearised)
+[lambdaProperties.lambda1Star,lambdaProperties.lambda2Star] = ...
+    lambdaSearchLinear(momentData.moment1Matrix,...
+    momentData.moment2Matrix,thetaProperties.rMatrix);
+
+% Estimate functions f and g (drift and noise)
+lambda1_1 = lambdaProperties.lambda1Star(1,:); % Array of lambda^(1)_1
+lambda2_1 = lambdaProperties.lambda2Star(1,:); % Array of lambda^(2)_1
+[fEstimate,gEstimate,fInitial,gInitial] = fgIter(lambda1_1,lambda2_1,...
+    thetaStar,momentData.evalPoints,fitOptions.functionConvergence);
+
+% Mean fit error
+meanAbsoluteError = meanFitError(momentData,thetaProperties,...
+    lambdaProperties,fitOptions.printOutput);
+
+%% Making object
+outSPmodelObject = SPmodelClass(thetaStar,fEstimate,gEstimate,fInitial,...
+    gInitial,meanAbsoluteError,momentData,thetaProperties,...
+    lambdaProperties,fitOptions);
+end
+function meanAbsoluteError = meanFitError(momentData,thetaProperties,...
+    lambdaProperties,printOutput)
+%Calculating mean fit error
+%   William Davis, 22/01/20
+%
+%   Notes:
+%   Calculating mean error between data and fit
+%
+%   Inputs:
+%   - "momentData"              Moment data object, MomentClass
+%   - "thetaProperties"         Correlation time properties, structure
+%   - "lambdaProperties"        Function fit properties, structure
+%   - "printOutput"             Print to command window? logical
+%
+%   Problems:
+%   - Add option to surpress print to terminal
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+moment1Difference = momentData.moment1Matrix-...
+    thetaProperties.rMatrix*lambdaProperties.lambda1Star;
+moment2Difference = momentData.moment2Matrix-...
+    thetaProperties.rMatrix*lambdaProperties.lambda2Star;
+meanAbsoluteError.moment1 = mean(abs(moment1Difference(:)));
+meanAbsoluteError.moment2 = mean(abs(moment2Difference(:)));
+meanAbsoluteError.bothMoments = mean([meanAbsoluteError.moment1,...
+    meanAbsoluteError.moment2]);
+if printOutput
+    disp('Absolute fit error on moments')
+    disp(['Mean M^(1) fit error: ',...
+        sprintf('%0.4e',meanAbsoluteError.moment1)])
+    disp(['Mean M^(2) fit error: ',...
+        sprintf('%0.4e',meanAbsoluteError.moment2)])
+    disp(['Mean fit error: ',...
+        sprintf('%0.4e',meanAbsoluteError.bothMoments)])
+end
+end
+function [thetaStarNew,thetaProperties] = theta_search(X,dt,nuMax,...
+    thetaMax,betaConv)
+%Theta search
+%   William Davis, 13/01/19
+%
+%   Notes:
+%   Non-linear searching for theta, the correlation time of a stochastic
+%   process. Fitting the autocorrelation increments to basis functions.
+%   Two searches are made.
+%   
+%   Tau from 1 to tau_max will be used in the first search, and once theta
+%   is estimated, theta becomes the tau_max for the second search. The
+%   second theta is used, due to scaling arguments.
+%
+%   Altered to incorporate multiple data sources.
+%   Grouped outputs into "thetaProperties".
+%
+%   Inputs:
+%   - "X"                       Observed variable, cell array of data
+%   - "dt"                      Time-step
+%   - "nu_max"                  Index of maximum time-shift
+%   - "theta_max"               Maximum correlation time in search
+%   - "beta_conv"               Convergence parameter (not used yet)
+%
+%   Problems:
+%   - See note on nu correction.
+%   - Tidy up
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Processing
+% Autocorrelation
+dA = nAutocorrIncrement(X,nuMax); % Multiple data
+
+% First search
+[thetaStarInitial,rNuMatrix,~] = thetaBasisFunctionFit(dA,dt,nuMax,...
+    thetaMax,betaConv);
+
+% New maximum tau
+newNuMax = ceil(sqrt(thetaStarInitial)/dt);
+
+if newNuMax > nuMax
+    % I think nu needs to be as small as possible.
+    % The case of taking a new nu max might only be appropriate for time-
+    % series data that has bigger time steps. For our purposes, the time-
+    % step is quite small, relative to the characteristic time-scale of the
+    % process. If we do use the new nu maximum, what effectively happens is
+    % the estimates from lambda get dominated by the first r function (the
+    % linear term). This overpowers the other two terms, and means that the
+    % algorithm cannot sense the correlation. I.e. the theta_bf_fit
+    % function for the new nu maximum has a minimum very close to 0, as
+    % opposed to the original theta_bf_fit run that has a finite time
+    % minimum. This may need further investigation.
+    
+    newNuMax = nuMax;
+    
+    dA = nAutocorrIncrement(X,newNuMax);
+    
+end
+
+% Second search
+newThetaMax = newNuMax*dt; % New theta maximum
+[thetaStarNew,~,lambdaStar] = thetaBasisFunctionFit(dA(1:newNuMax),dt,...
+    newNuMax,newThetaMax,betaConv);
+
+% R matrix
+rMatrix = rNuMatrix(thetaStarNew); % Output r(tau,theta) matrix
+
+%% Outputs
+thetaProperties.newNuMax = newNuMax;
+thetaProperties.rMatrix = rMatrix;
+thetaProperties.dA = dA;
+thetaProperties.lambdaStar = lambdaStar;
+end
+function [dA_full] = nAutocorrIncrement(X,tauMax)
+%Autocorrelation increments, n-data
+%   William Davis, 09/07/19
+%
+%   Notes:
+%   Calculates the (non-normalised) autocorrelation increments for a given
+%   maximum time-shift. Calculates increments from n sources of data.
+%
+%   Inputs:
+%   - "X"                       Observed variable, cell array of data
+%   - "nu_max"                  Index of maximum time-shift
+%
+%   Problems:
+%   - 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Processing
+ndata = numel(X); % Number of data-sets
+
+[acf,dA] = deal(cell(1,ndata)); % Preallocate
+[nX,varX] = deal(zeros(1,ndata));
+for nd = 1:ndata
+    % Built in function (faster)
+    [acf{nd},~,~] = autocorr(X{nd},'NumLags',tauMax); 
+    nX(nd) = numel(X{nd}); % Number of points
+    varX(nd) = var(X{nd}); % Variance
+    dA{nd} = (acf{nd}(2:end)-acf{nd}(1))*var(X{nd}); % Tidy up array
+end
+
+%% Merge data
+dAmat = cell2mat(dA); % Make into matrix
+dA_full = (dAmat*nX')/sum(nX);
+
+% Old code
+%[acf,~,~] = autocorr(X,'NumLags',tau_max); % Built in function (faster)
+%dA = (acf(2:end)-acf(1))*var(X); % Tidy up array
+end
+function [thetaStar,rNuMatrix,lambdaStar] = thetaBasisFunctionFit(dA,dt,...
+    nuMax,thetaMax,betaConv)
+%Theta basis function fit
+%   William Davis, 12/01/19
+%
+%   Notes:
+%   Non-linear searching for theta, the correlation time of a stochastic
+%   process. Fits autocorrelation to basis functions to find optimal theta.
+%   Assumes all tau from 1 to tau_max will be used in search.
+%   
+%
+%   Inputs:
+%   - "dA"                      Autocorrelation increments
+%   - "dt"                      Time-step
+%   - "nu_max"                  Index of maximum time-shift
+%   - "theta_max"               Maximum correlation time in search
+%   - "beta_conv"               Convergence parameter (not used)
+%
+%   Problems:
+%   - 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Processing
+nu = 1:nuMax; % Indexes of time-shifts
+tau_nu = nu'*dt; % Array of time-shifts
+
+% Basis functions
+r1 = @(tau,theta) tau - theta*(1 - exp(-tau/theta));
+r2 = @(tau,theta) tau.^2/2 - theta*r1(tau,theta);
+r3 = @(tau,theta) tau.^3/6 - theta*r2(tau,theta);
+rArray = @(tau,theta) [r1(tau,theta),r2(tau,theta),r3(tau,theta)];
+
+% Functions r matrix (reducing dependencies, new method)
+rNuMatrix = @(theta) rArray(tau_nu,theta);
+
+%% Searching for theta
+% Objective function
+lambdaFunc = @(theta_c) rNuMatrix(theta_c)\dA;
+funcValue = @(theta_c) sum((dA-rNuMatrix(theta_c)*lambdaFunc(theta_c)).^2);
+
+% Line search using golden section search and parabolic interpolation
+thetaStar = fminbnd(funcValue,0,thetaMax);
+
+% Best fit lambda vector
+lambdaStar = lambdaFunc(thetaStar);
+end
+function [fNew,gNew,f0,g0] = fgIter(lambda1_1,lambda2_1,theta,Xcentre,betaConv)
+%Iterate functions f and g
+%   William Davis, 14/01/19
+%
+%   Notes:
+%   Iterate and converge for functions f and g, based on estimates for
+%   lambda vectors. Input x positions are needed since spatial derivatives
+%   are calculated in fixed point iterations.
+%
+%   Inputs:
+%   - "lambda1_1"               Array of lambda^(1)_1, function of x
+%   - "lambda2_1"               Array of lambda^(2)_1, function of x
+%   - "theta"                   Estimate of correlation time
+%   - "Xcentre"                 Position of x estimates
+%   - "beta_conv"               Convergence parameter
+%
+%   Problems:
+%   - Tidy up
+%   - Convergence parameter too small?
+%   - Write better convergence algorithm?
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Settings
+count_max = 1; % Iteration limits
+
+%% Processing
+% Starting values
+f0 = lambda1_1; % Drift function
+g0 = sqrt(abs(lambda2_1)); % Noise function
+fNew = f0;
+gNew = g0;
+
+% Plotting for for debugging purposes
+%figure(1),hold on
+%plot(Xcentre,f_0,'k-','LineWidth',2),xlabel('X'),ylabel('f')
+%figure(2),hold on
+%plot(Xcentre,g_0,'k-','LineWidth',2),xlabel('X'),ylabel('f')
+
+% Iterate until converged
+count = 0; % Initial count
+int_err = inf; % Initial error
+while (int_err > betaConv) && (count < count_max)
+    % Update counter
+    count = count + 1; 
+    
+    % Update functions
+    f_old = fNew; 
+    g_old = gNew;
+    
+    % Find new values
+    [fNew,gNew] = fixedPointIter(lambda1_1,lambda2_1,f_old,g_old,theta,...
+        Xcentre);
+    
+    % Integration error
+    int_err = trapz(Xcentre,(fNew-f_old).^2) + ...
+        trapz(Xcentre,(gNew-g_old).^2);
+    
+    % Plotting for for debugging purposes
+    %figure(1),plot(Xcentre,f_new),
+    %figure(2),plot(Xcentre,g_new)
+    
+end
+
+end
+function [lambda1,lambda2] = lambdaSearchLinear(M1,M2,r_matrix)
+%Lambda search
+%   William Davis, 29/05/19
+%
+%   Notes:
+%   Linear searching for lambda, the correlation time of a stochastic
+%   process. Lambdas are calculated as a function of x position, although
+%   the specific values of the positions are not needed. Only the size of
+%   the x position vector is needed. Linearised, faster.
+%
+%   Inputs:
+%   - "M1"                      First moments
+%   - "M2"                      Second moments
+%   - "r_matrix"                Function r(tau,theta) matrix
+%
+%   Problems:
+%   - Use original tau_max or second tau_max?
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Loop over requested x points
+nx_points = size(M1,2); % Number of x points
+
+lambda1 = zeros(3,nx_points); % Preallocating lambda^(1) array
+lambda2 = zeros(3,nx_points); % Preallocating lambda^(2) array
+
+for ii = 1:nx_points
+    % Minimising normal matrix
+    lambda1(:,ii) = r_matrix\M1(:,ii); 
+    lambda2(:,ii) = r_matrix\M2(:,ii);
+end
+end
+function [fNew,gNew] = fixedPointIter(lambda1_1,lambda2_1,f,g,theta,...
+    Xcentre)
+%Fixed point iteration
+%   William Davis, 14/01/19
+%
+%   Notes:
+%   Fixed point iteration to update functions f and g. Assumes a small
+%   theta.
+%   
+%
+%   Inputs:
+%   - "lambda1_1"               Array of lambda^(1)_1, function of x
+%   - "lambda2_1"               Array of lambda^(2)_1, function of x
+%   - "f"                       Array, function f estimate
+%   - "g"                       Array, function g estimate
+%   - "theta"                   Estimate of correlation time
+%   - "Xcentre"                 Position of x estimates
+%
+%   Problems:
+%   - 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Processing
+% Spatial f and g derivates
+%f_div = gradient(f,Xcentre);
+%g_div = gradient(g,Xcentre);
+f_div = fdiffNU(Xcentre,f);
+g_div = fdiffNU(Xcentre,g);
+
+% Updated functions
+fNew = lambda1_1 - 0.5*g.*g_div - ...
+    0.5*theta*(f_div.*g.*g_div - f.*g_div.^2);
+g_arg = lambda2_1 - theta*(f_div.*g.^2 - f.*g.*g_div);
+gNew = sqrt(abs(g_arg));
+
+end
+function dFdx = fdiffNU(x,F)
+%Irregular grid derivative
+%   William Davis, 01/05/19
+%
+%   Notes:
+%   Based off code from Bruce.
+%   Polynomial interpolation and finite difference differentiation for 
+%   irregular grid spacing. Notes at:
+%
+%   http://cfd.mace.manchester.ac.uk/twiki/pub/Main/...
+%                   TimCraftNotes_All_Access/cfd1-findiffs.pdf
+%
+%   Second order accuracy. Three point centered stencil for body and three
+%   point offset stencil for boundary points. Vectorised for efficiency.
+%   Checked by hand and numerically.
+%
+%   Input parameters:
+%   - "x"                           Abscissa
+%   - "F"                           Function values
+%
+%   Problems:
+%   - Write second order code?
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Processing
+n = length(x); % Length
+m = n-2; % End point for body
+
+h0 = x(2:n-1) - x(1:n-2); % Backwards steps
+h1 = x(3:n) - x(2:n-1); % Forward steps
+den = h0.*h1.*(h0+h1); % Denominator
+h0_2 = h0.^2; % Backwards steps squared
+h1_2 = h1.^2; % Forward steps squared
+
+% Boundaries
+dFdx(n) = ((h0_2(m)+2*h0(m)*h1(m))*F(n) - ...
+    (h0(m)+h1(m))^2*F(n-1) + h1_2(m)*F(n-2))/den(m);
+dFdx(1) = (-(h1_2(1) + 2*h0(1)*h1(1))* F(1) + ...
+    (h0(1)+h1(1))^2*F(2) - h0_2(1)*F(3))/den(1);
+
+% Main Body
+dFdx(2:n-1)= (-h1_2.*F(1:n-2) + (h1_2-h0_2).*F(2:n-1) + h0_2.*F(3:n))./den;
+end
